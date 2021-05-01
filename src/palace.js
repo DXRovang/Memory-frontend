646b5673f8ec0df12e74426467e9d4471accb0a4
Alexander Rovang <wolf_vii@yahoo.com>
2021-05-01 16:12:20 -0400

    adds postPalace

--- src/palace.js
+++ src/palace.js
@@ -9,31 +9,15 @@ function fetchPalaces(){
 }
 
 // find a DOM element and attach to it
-// function appendPalace(palaces){
-//   const palaceDiv = document.getElementById("Title")
-//   for (let palace of palaces){
-//     const ul = document.createElement("ul")
-//     ul.innerHTML = palace.name
-//     palaceDiv.append(ul)
-//     // appendLoci(palaces)
-//     // appendItem(palaces)
-//   }
-//   appendLoci(palaces)
-//   appendItem(palaces)
-// }
-
-// option 2 - less code but no separation
 function appendPalace(palaces){
   const palaceDiv = document.getElementById("Title")
-  const lociDiv = document.getElementById("Locis")
-  const itemDiv = document.getElementById("Item")
 
   for (let palace of palaces){
     const ul = document.createElement("ul")
     ul.innerHTML = palace.name
     palaceDiv.append(ul)
+
     for(let i = 0; i < palace.locis.length; i++){
-      
       const li = document.createElement("li")
       const le = document.createElement("li")
       li.setAttribute("class", "loci")
@@ -43,13 +27,24 @@ function appendPalace(palaces){
       le.innerHTML = palace.locis[i].item
       palaceDiv.append(li)
       palaceDiv.append(le) 
-    } 
-
-   
+    }  
   }
 }
 
+// VERY tricky, look at this!!!
 function postPalace(e){
   e.preventDefault()
   const userInput = e.target.children[1].value
-}
+  const options = {
+    method: "POST", 
+    headers: {
+      "Content-type": "application/json",
+      Accept: "application/json"
+    },
+    body: JSON.stringify({palace: {name: userInput}})
+    }
+    fetch("http://localhost:3000/palaces", options)
+    .then(r => r.json())
+    .then(palace => console.log(palace))
+  }
+
