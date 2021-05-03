9d9932b4eda76d2b946b829b30d004583a9e8116
Alexander Rovang <wolf_vii@yahoo.com>
2021-05-02 22:28:18 -0400

    moves appendLocis to locis.js

--- src/palace.js
+++ src/palace.js
@@ -7,46 +7,17 @@ class Palace{
     this.id = palace.id
     this.locis = palace.locis
   }
-  // adds a single palace > try & split this into append loci & item
+
   appendPalace(){
     const palaceDiv = document.getElementById("Title")
     const ul = document.createElement("ul")
     ul.innerHTML = this.name
     palaceDiv.append(ul)
 
-    for(let i = 0; i < this.locis.length; i++){
-      const li = document.createElement("li")
-      const le = document.createElement("li")
-
-      li.setAttribute("class", "loci")
-      le.setAttribute("class", "item")
-  
-      li.innerHTML = this.locis[i].name
-      palaceDiv.append(li)
-      le.innerHTML = this.locis[i].item
-      palaceDiv.append(le) 
-
-      const bi = document.createElement("br")
-      const be = document.createElement("br")
-
-      const liDelete = document.createElement("button")
-      liDelete.innerText = "Delete"
-      // got lost here, CRD functionality 14:59
-      liDelete.addEventListener("click", function(e){
-        deleteLi(this.id, li)})
-      li.append(bi)
-      li.append(liDelete)
-
-      const leDelete = document.createElement("button")
-      leDelete.innerText = "Delete"
-      leDelete.addEventListener("click", function(e){
-        deleteLe(this.id, le)})
-      le.append(be)
-      le.append(leDelete)
-    }
+    appendLocis(this, palaceDiv)
   }
 
-  // static are related to class methods in ruby
+  // static are like class methods in ruby
   static fetchPalaces(){
     fetch("http://localhost:3000/palaces")
     // converts response from from json into js, no return necessary if no curly brackets
@@ -65,7 +36,38 @@ class Palace{
 
 }
 
+function appendLocis(palace, palaceDiv){
+  for(let i = 0; i < palace.locis.length; i++){
+    const li = document.createElement("li")
+    const le = document.createElement("li")
 
+    li.setAttribute("class", "loci")
+    le.setAttribute("class", "item")
+
+    li.innerHTML = palace.locis[i].name
+    palaceDiv.append(li)
+    le.innerHTML = palace.locis[i].item
+    palaceDiv.append(le) 
+
+    const bi = document.createElement("br")
+    const be = document.createElement("br")
+
+    const liDelete = document.createElement("button")
+    liDelete.innerText = "Delete"
+    // got lost here, CRD functionality 14:59
+    liDelete.addEventListener("click", function(e){
+      deleteLi(palace.id, li)})
+    li.append(bi)
+    li.append(liDelete)
+
+    const leDelete = document.createElement("button")
+    leDelete.innerText = "Delete"
+    leDelete.addEventListener("click", function(e){
+      deleteLe(palace.id, le)})
+    le.append(be)
+    le.append(leDelete)
+  }
+}
 
 
 function deleteLi(palaceId, li){
